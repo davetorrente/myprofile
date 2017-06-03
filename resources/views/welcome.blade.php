ab15ba4a310b993c848979b4c16fb5c6f1bd9644
davetorrente <whaepekk@gmail.com>
2017-06-03 15:13:00 -0700

    fixExp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,18 +122,18 @@
                 </div>
                 <div class="col-sm-8 col-sm-offset-1">
                     <div class="row row-space-2 margin-b-4">
-                        <div class="col-md-4 md-margin-b-4">
-                            <div class="service" data-height="height">
-                                <div class="service-element">
-                                    <i class="service-icon icon-chemistry"></i>
-                                </div>
-                                <div class="service-info">
-                                    <h3>HTML5/CSS3</h3>
-                                    <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor</p>
-                                </div>
+                        {{--<div class="col-md-4 md-margin-b-4">--}}
+                            {{--<div class="service" data-height="height">--}}
+                                {{--<div class="service-element">--}}
+                                    {{--<i class="service-icon icon-chemistry"></i>--}}
+                                {{--</div>--}}
+                                {{--<div class="service-info">--}}
+                                    {{--<h3>HTML5/CSS3</h3>--}}
+                                    {{--<p class="margin-b-5"></p>--}}
+                                {{--</div>--}}
                                 {{--<a href="#" class="content-wrapper-link"></a>--}}
-                            </div>
-                        </div>
+                            {{--</div>--}}
+                        {{--</div>--}}
                         <div class="col-md-4 md-margin-b-4">
                             <div class="service bg-color-base wow zoomIn" data-height="height" data-wow-duration=".3" data-wow-delay=".1s">
                                 <div class="service-element">
@@ -141,7 +141,7 @@
                                 </div>
                                 <div class="service-info">
                                     <h3 class="color-white">PHP Laravel</h3>
-                                    <p class="color-white margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor</p>
+                                    <p class="color-white margin-b-5">As an intern in Republisys, I've completed backend tasks of their projects and simple designs.</p>
                                 </div>
                                 {{--<a href="#" class="content-wrapper-link"></a>--}}
                             </div>
@@ -153,7 +153,7 @@
                                 </div>
                                 <div class="service-info">
                                     <h3>Node JS</h3>
-                                    <p class="margin-b-5">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor</p>
+                                    <p class="margin-b-5">We develop several projects using Node JS in heroku as instructed by our professor</p>
                                 </div>
                                 {{--<a href="#" class="content-wrapper-link"></a>--}}
                             </div>
